00be93867ee83d5ce2eb1ab317122b56b13d7e44
ahmed kareem <ahmed.karem211@yahoo.com>
2020-06-20 14:09:40 +0200

    Add Viewe Models

---
+++ FinalProject/FinalProject/ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using FinalProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<product> products { get; set; }
+        public List<mainCategory> mainCategories{ get; set; }
+        public List<subCategory> subCategories { get; set; }
+
+    }
+}
