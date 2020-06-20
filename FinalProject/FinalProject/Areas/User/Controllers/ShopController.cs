e295b13a1b13b94e6bca881e141f9674e216e047
ahmed kareem <ahmed.karem211@yahoo.com>
2020-06-20 21:46:50 +0200

    Add Shop View

---
+++ FinalProject/FinalProject/Areas/User/Controllers/ShopController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Areas.User.Controllers
+{
+    [Area("User")]
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public ShopController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult ShopProducts(int SubCategoryId)
+        {
+            //var subCategory = context.SubCategories.Find(SubCategoryId);
+            var products = context.Products.Where(p=>p.subCategoryId == SubCategoryId);
+
+            return View(products);
+        }
+    }
+}
